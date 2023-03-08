5a222c00e05cc3041cad818046ac59e636cd9c6e
HanHan195 <henrytran.195@gmail.com>
2023-03-08 17:20:55 +0700

    FisrtDegreeEqution

---
+++ IntrodutiontoJava/src/main/java/FisrtDegreeEquation.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class FisrtDegreeEquation {
+    //giải phương trình bậc nhất ax + b = c
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        //nhập giá trị  a b c
+        System.out.println("a : ");
+        double a = scanner.nextDouble();
+
+        System.out.println("b : ");
+        double b = scanner.nextDouble();
+
+        System.out.println("c :");
+        double c = scanner.nextDouble();
+        //tìm x
+        if(a != 0){
+            double answer = (c - b )/ a;
+            System.out.printf("Equation pass with x = %f!\n", answer);
+        } else {
+            if(b==c){
+                System.out.println("The solution is all x!");
+            } else {
+                System.out.println("NO solution!");
+            }
+        }
+
+
+
+
+
+
+    }
+}
